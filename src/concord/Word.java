4cd586008373102d374b6bb47e1369875ebecfac
loganrohde <logan.rohde@gmail.com>
2016-02-25 22:02:57 -0500

    Updated word class
    It now has the frequency and an array list of each line that the word appears on pending that we can iterate through each line

--- src/concord/Word.java
+++ src/concord/Word.java
@@ -12,6 +12,7 @@ public class Word{
 	
 	private final String word;
     private int frequency;
+    private ArrayList<Integer> lines;
     
     /**
      * Make a new word with the given string; frequency initialized to 1.
@@ -20,5 +21,52 @@ public class Word{
     public Word(String word) {
         this.word = word;
         this.frequency = 1;
+        lines = new ArrayList<>();
+    }
+    
+     /**
+     * Uses the frequency of each word and compares in order to see if word is
+     * in the concordance already
+     *
+     * @param other
+     * @return difference
+     */
+    @Override
+    public int compareTo(Object other) {
+        Word otherWord = (Word) other;
+        int difference = otherWord.frequency - this.frequency;
+        if (difference == 0) {
+            difference = this.word.compareTo(otherWord.word);
+        }
+        return difference;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return this.word.equals(((Word) other).word);
+    }
+
+    @Override
+    public String toString() {
+        return word + " appears " + frequency + " times.";
+    }
+    
+    public void addLine(int line){
+        lines.add(line);
+    }
+    
+    public ArrayList<Integer> getLines() {
+        return lines;
+    }
+    
+    public String getWord() {
+        return word;
+    }
+
+    /**
+     * increment the frequence of the word by one
+     */
+    public void increment() {
+        frequency++;
     }
 }
