3ac02900be812b5de0ef698b658c0a4bb7412aa7
wrforresUNCG <wrforres@uncg.edu>
2016-03-04 02:26:54 -0500

    Update TextRepo.java 0226 160304

--- src/concord/TextRepoOp.java
+++ src/concord/TextRepoOp.java
@@ -8,19 +8,44 @@ import java.util.*;
  */
 public class TextRepoOp {
 
-	public static String showTitles(){
-		String titleList = "This would show titles";
+	public static String showTitles(File dir){
+		//dir = dir;
+		String titleList = "Locally Stored Titles:";
+		String title = "";
+		File[] dirContents = dir.listFiles();
+		int i = 0;
+		while(i < dirContents.length){
+			title = dirContents[i].toString();
+			title = title.substring(title.lastIndexOf('\\') + 1, title.indexOf('.'));
+			titleList = titleList + "\n" + title;
+			i++;
+		}
 		return titleList;
 	}
 	
-	public static String showTitlesKeyword(String keyword){
+	public static String showTitlesKeyword(File dir, String keyword){
 		keyword = keyword.trim();
-		String titleList = "This would show titles with '" + keyword + "' in it";
+		String titleList = "Locally Stored Titled With Keyword '" + keyword + "':";
+		String title = "";
+		File[] dirContents = dir.listFiles();
+		int i = 0;
+		while(i < dirContents.length){
+			title = dirContents[i].toString();
+			title = title.substring(title.lastIndexOf('\\') + 1, title.lastIndexOf('.'));
+			if(title.contains(keyword)){
+				titleList = titleList + "\n" + title;
+			}
+			i++;
+		}
 		return titleList;
 	}
 	
 	public static String showTitlesAuthor(String author){
 		String titleList = "This would show titles by '" + author + "'";
+		titleList = titleList + "\n... but we haven't implemented that functionality yet";
+		titleList += "\nStripping titles from Gutenberg is on the way, but the current file";
+		titleList += "\nlabeling system does not include authors. Then again, there format";
+		titleList += "\nuses numbers rather than titles. So it's all clearly obfuscated.";
 		return titleList;
 	}
 }
